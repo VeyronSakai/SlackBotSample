23e47a1b462678ca1cfbd66961a222fe5c7d94d7
VeyronSakai <yuukisakai123@gmail.com>
2021-03-08 00:42:25 +0900

    fix token

--- main.go
+++ main.go
@@ -48,8 +48,8 @@ func main() {
 			innerEvent := eventsAPIEvent.InnerEvent
 			switch event := innerEvent.Data.(type) {
 			case *slackevents.AppMentionEvent:
-				if strings.Contains(event.Text, "山") {
-					if _, _, err := api.PostMessage(event.Channel, slack.MsgOptionText("川!", false)); err != nil {
+				if strings.Contains(event.Text, "や") {
+					if _, _, err := api.PostMessage(event.Channel, slack.MsgOptionText("こんにちは。これはテストです。", false)); err != nil {
 						log.Println(err)
 						w.WriteHeader(http.StatusInternalServerError)
 						return
