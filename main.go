716447e6f478855bd12b6ac27fef80724a685125
VeyronSakai <yuukisakai123@gmail.com>
2021-03-07 23:54:39 +0900

    fix token

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 )
 
 func main() {
-	api := slack.New("xoxb-541408309975-1843511741569-zIlrO6nUNIJM5wbo6aibyUZZ")
+	api := slack.New("xoxb-541408309975-1843511741569-73WKpZHGQJq31BbmvgocqwZr")
 
 	http.HandleFunc("/slack/events", func(w http.ResponseWriter, r *http.Request) {
 		body, err := ioutil.ReadAll(r.Body)
