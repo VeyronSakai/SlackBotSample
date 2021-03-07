1418b452e44664cf64d489fabf670476d1b2ffab
VeyronSakai <yuukisakai123@gmail.com>
2021-03-07 23:14:50 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	api := slack.New("xoxb-541408309975-1843511741569-zIlrO6nUNIJM5wbo6aibyUZZ")
+
+	http.HandleFunc("/slack/events", func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		eventsAPIEvent, err := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionNoVerifyToken())
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		switch eventsAPIEvent.Type {
+		case slackevents.URLVerification:
+			var res *slackevents.ChallengeResponse
+			if err := json.Unmarshal(body, &res); err != nil {
+				log.Println(err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "text/plain")
+			if _, err := w.Write([]byte(res.Challenge)); err != nil {
+				log.Println(err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+		case slackevents.CallbackEvent:
+			innerEvent := eventsAPIEvent.InnerEvent
+			switch event := innerEvent.Data.(type) {
+			case *slackevents.AppMentionEvent:
+				if strings.Contains(event.Text, "エヴァ") {
+					if _, _, err := api.PostMessage(event.Channel, slack.MsgOptionText("うーん、面白くないんじゃない？", false)); err != nil {
+						log.Println(err)
+						w.WriteHeader(http.StatusInternalServerError)
+						return
+					}
+				}
+			}
+		}
+	})
+
+	log.Println("[INFO] Server listening")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
